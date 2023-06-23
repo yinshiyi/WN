011099a5c13861a0b2894e6ed5ea0510ad9c40c6
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2023-06-23 11:22:01 -0700

    Update url.py
    adding the loops

--- url.py
+++ url.py
@@ -1,15 +1,31 @@
+# url_month = "https://www.southwest.com/air/low-fare-calendar/select-dates.html?adultPassengersCount=1&currencyCode=POINTS&destinationAirportCode=HNL&hasNearByAirport=undefined&lapInfantPassengersCount=0&originationAirportCode=SJC&passengerType=ADULT&promoCode=&returnAirportCode=&selectedFlight1=2023-09-06&selectedFlight2=&tripType=oneway&clk=6403032&cbid=6403032"
+# para['fareType'] = ['USD']
 from urllib.parse import urlparse, parse_qs, parse_qsl, urlunparse, urlencode
 
+# Initial URL
 url = "https://www.southwest.com/air/booking/select.html?adultPassengersCount=1&departureDate=2023-09-23&departureTimeOfDay=ALL_DAY&destinationAirportCode=SEA&fareType=POINTS&int=LFCBOOKAIR&lapInfantPassengersCount=0&originationAirportCode=OAK&passengerType=ADULT&promoCode=&returnAirportCode=&returnDate=2023-09-24&returnTimeOfDay=ALL_DAY&selectedFlight1=2023-09-23&selectedFlight2=2023-09-24&tripType=roundtrip"
-url_month = "https://www.southwest.com/air/low-fare-calendar/select-dates.html?adultPassengersCount=1&currencyCode=POINTS&destinationAirportCode=HNL&hasNearByAirport=undefined&lapInfantPassengersCount=0&originationAirportCode=SJC&passengerType=ADULT&promoCode=&returnAirportCode=&selectedFlight1=2023-09-06&selectedFlight2=&tripType=oneway&clk=6403032&cbid=6403032"
+
+# Parse the URL
 o = urlparse(url)
 para = parse_qs(o.query)
 
-para['fareType'] = ['USD']
-para['destinationAirportCode'] = ['HNL','KOA','LIH']
-para['originationAirportCode'] = ['OAK','SJC','SFO']
+# Combinations for destinationAirportCode and originationAirportCode
+destinations = ['HNL', 'KOA', 'LIH']
+origins = ['OAK', 'SJC', 'SFO']
 
-query_string = urlencode(para,doseq=True)
+# Generate 9 links
+links = []
+for dest in destinations:
+    for origin in origins:
+        # Update the query parameters
+        para['destinationAirportCode'] = [dest]
+        para['originationAirportCode'] = [origin]
 
-updated_url = urlunparse([o[0], o[1], o[2], o[3], query_string, o[5]])
-print(updated_url)
+        # Generate the updated URL
+        query_string = urlencode(para, doseq=True)
+        updated_url = urlunparse([o[0], o[1], o[2], o[3], query_string, o[5]])
+        links.append(updated_url)
+
+# Print the generated links
+for link in links:
+    print(link)
